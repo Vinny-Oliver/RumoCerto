45ddff38197740d491f1599278cc9767ee4522df
Vinny-Oliver <135837794+Vinny-Oliver@users.noreply.github.com>
2025-02-09 21:01:13 -0300

    atualização do banco de dados

--- db/banco.sql
+++ db/banco.sql
@@ -1,30 +1,21 @@
-CREATE TABLE Destinos (
-  id_destino INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Clientes (
+  id_cliente INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
-  descricao TEXT,
-  imagem VARCHAR(255)
+  email VARCHAR(100) UNIQUE NOT NULL,
+  telefone VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Pacotes (
   id_pacote INT AUTO_INCREMENT PRIMARY KEY,
-  id_destino INT,
-  nome_pacote VARCHAR(100),
-  preco DECIMAL(10, 2),
-  FOREIGN KEY (id_destino) REFERENCES Destinos(id_destino)
-);
-
-CREATE TABLE Clientes (
-  id_cliente INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(100),
-  email VARCHAR(100) UNIQUE,
-  telefone VARCHAR(20)
+  nome_pacote VARCHAR(100) NOT NULL,
+  preco DECIMAL(10,2) NOT NULL
 );
 
 CREATE TABLE Reservas (
   id_reserva INT AUTO_INCREMENT PRIMARY KEY,
-  id_cliente INT,
-  id_pacote INT,
-  data_reserva DATETIME,
-  FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
-  FOREIGN KEY (id_pacote) REFERENCES Pacotes(id_pacote)
+  id_cliente INT NOT NULL,
+  id_pacote INT NOT NULL,
+  data_reserva DATETIME NOT NULL,
+  FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente) ON DELETE CASCADE,
+  FOREIGN KEY (id_pacote) REFERENCES Pacotes(id_pacote) ON DELETE CASCADE
 );
